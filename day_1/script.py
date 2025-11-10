234685ae65f847345f3007f9d58bdd1967dec6cc
Csolatus <lucas.lanoue@efrei.net>
2025-11-10 11:44:42 +0100

    Day 1 of 30 days of Python

---
+++ day_1/script.py
@@ -0,0 +1,37 @@
+from math import *
+
+print("Hello World !")
+
+#exerice 1
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+print("Lucas")
+print("Lanoue")
+print("France")
+print("I am enjoying 30 days of pyhton")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Lucas"))
+print(type("Lanoue"))
+print(type("France"))
+
+#exercice 3
+print(3, 3.14, 3-3j)
+print("Bonjour")
+print(True)
+print(["Lucas", "Lanoue"])
+print((9.8, 3.14, 2.2))
+print({2.1, 3.2, 9.9})
+print({'Lanoue', 'Lucas'})
+
+print (sqrt((10-2)**2 + (8-3)**2))
