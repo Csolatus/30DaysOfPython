30f360427013d09d9080adda1d7f1a20efa89eb9
Csolatus <lucas.lanoue@efrei.net>
2025-12-06 18:54:57 +0100

    Day 3

---
+++ day_3/script.py
@@ -0,0 +1,81 @@
+import math
+
+age = 20
+my_height = 1.75
+complex_number = 2j
+
+#base = int(input('Enter base : '))
+#height = int(input('Enter height : '))
+#triangle_area = 0.5*base*height
+#stringable_triangle_area = str(triangle_area)
+#print('Triangle area is ' + stringable_triangle_area)
+
+#a = int(input('Enter side a : '))
+#b = int(input('Enter side b : '))
+#c = int(input('Enter side c : '))
+#triangle_perimeter = a+b+c
+#stringable_triangle_perimeter = str(triangle_perimeter)
+#print('The perimeter of the triangle is ' + stringable_triangle_perimeter)
+
+#length = int(input('Enter length : '))
+#width = int(input('Enter width : '))
+#rectangle_area = length * width
+#rectangle_perimeter = 2 * (length+width)
+#stringable_rectangle_area = str(rectangle_area)
+#stringable_rectangle_perimeter = str(rectangle_perimeter)
+#print('The rectangle area is : ' + stringable_rectangle_area)
+#print('The rectangle perimeter is : ' + stringable_rectangle_perimeter)
+
+#radius = int(input('Enter radius : '))
+#circle_area = math.pi*radius**2
+#circle_circumference = 2*math.pi*radius
+#stringable_circle_circumference = str(circle_circumference)
+#stringable_circle_area = str(circle_area)
+#print('The circle area is : ' + stringable_circle_area)
+#print('The circle circumference : ' + stringable_circle_circumference)
+
+#print(len('python'))
+#print(len('dragon'))
+#print(len('dragon') == len('python'))
+
+#print('jargon' in 'I hope this course is not full of jargon')
+
+#print('on' in 'dragon' and 'on' in 'python')
+
+#len_python = len('python')
+#float_python = float(len_python)
+#string_python = str(float_python)
+
+#a = int(input('give number a : '))
+#print(a%2==0)
+
+#b = 2.7
+#int_b = int(b)
+#print(7//3 == int_b)
+
+#a = '10'
+#b = 10
+#print(a == b)
+
+#a = float('9.8')
+#int_a = int(a)
+#b = 10
+#print(a == b)
+
+#hours = int(input('Enter hours : '))
+#rate = int(input('Enter rate per hours : '))
+#earnings = hours * rate
+#stringable_earnings = str(earnings)
+#print('Your weekly earnings is ' + stringable_earnings)
+
+#years = int(input('Enter number of years you have lived : '))
+#seconds = years * 31556952
+#stringable_seconds = str(seconds)
+#print('You have lived for ' + stringable_seconds + ' seconds.')
+
+print('1 1 1 1 1')
+print('2 1 2 4 8')
+print('3 1 3 9 27')
+print('4 1 4 16 64')
+print('5 1 5 25 125')
+
